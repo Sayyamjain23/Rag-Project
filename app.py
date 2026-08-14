33f1cc561d68512e3ee8d9124f1acf14d978d2a3
Sayyam Jain <sayyamjain535@gmail.com>
2026-08-14 17:33:43 +0530

    feat(pipeline): add PDF text extraction and character text splitter

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
 import streamlit as st
 from dotenv import load_dotenv
 from htmlTemplates import css, bot_template, user_template
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return text
+
+def get_text_chunks(raw_text):
+    text_splitter=characterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks= text_splitter.split_text(raw_text)
+    return chunks
+
+
+
+
+
+
+
+
+
 
 
 
@@ -22,6 +52,10 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing"):
                 st.write("Processing your documents...")
+                raw_text= get_pdf_text(pdf_docs)
+                text_chunks= get_text_chunks(raw_text)
+                st.success("Processing complete!")
+
 
 if __name_ == "__main__":
     main()           
